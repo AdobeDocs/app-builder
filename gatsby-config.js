827b840a0338344367f55ad9f96638d1d4a9b155
Stephan Ringel <ringel@adobe.com>
2021-04-26 21:42:38 +0200

    Update root path

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     pages: [
       {
         "title": "Project Firefly",
-        "path": "https://stage.adobe.io/project-firefly"
+        "path": "/../"
       },
       {
         "title": "Documentation",
