2d43b721f35bddcdbb73ec498c1afb4ad89d1096
Stephan Ringel <ringel@adobe.com>
2021-07-21 11:34:52 +0200

    Remove support from top nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,6 @@ module.exports = {
       {
         "title": "Resources",
         "path": "resources"
-      },
-      {
-        "title": "Support",
-        "path": "support"
       }
     ],
     subPages: [
