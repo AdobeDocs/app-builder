28f5dbe390c886f285566331a40d0885ac86c438
Patrick Russell <prussell@adobe.com>
2025-04-14 13:31:21 -0700

    feat: add app_builder_guides and runtime_guides to left navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,45 +19,41 @@ module.exports = {
     pages: [
       {
         "title": "Overview",
-        "path": "intro_and_overview/app_builder_overview"
+        "path": "intro_and_overview/app_builder_overview.md"
       },
       {
         "title": "Getting Started",
-        "path": "getting_started"
+        "path": "getting_started/first_app.md"
       },
       {
         "title": "Guides",
-        "path": "guides"
+        "path": "guides/guides_index.md"
       },
       {
         "title": "Resources",
-        "path": "resources"
+        "path": "resources/index.md"
       }
     ],
     subPages: [
       {
         "title": "Overview",
-        "path": "intro_and_overview/app_builder_overview"
+        "path": "intro_and_overview/app_builder_overview.md"
       },
       {
-        "title": "How it works",
-        "path": "intro_and_overview/app_builder_overview/how_it_works.md"
+        "title": "What is App Builder",
+        "path": "intro_and_overview/what_is_app_builder.md"
       },
       {
-        "title": "How to Get Access",
-        "path": "intro_and_overview/app_builder_overview/getting_access.md"
-      },
-      {
-        "title": "App Builder and Adobe I/O Runtime",
-        "path": "intro_and_overview/app_builder_overview/firefly_and_runtime.md"
+        "title": "Business Case",
+        "path": "intro_and_overview/business_case.md"
       },
       {
         "title": "FAQ",
-        "path": "intro_and_overview/app_builder_overview/faq.md"
+        "path": "intro_and_overview/faq.md"
       },
       {
         "title": "Community",
-        "path": "intro_and_overview/app_builder_overview/community.md"
+        "path": "intro_and_overview/community.md"
       },
       {
         "title": "Creating your First App Builder App",
@@ -78,6 +74,126 @@ module.exports = {
           {
             "title": "References",
             "path": "guides/references.md"
+          },
+          {
+            "title": "App Builder Guides",
+            "path": "guides/app_builder_guides",
+            "pages": [
+              {
+                "title": "Architecture Overview",
+                "path": "guides/app_builder_guides/architecture_overview"
+              },
+              {
+                "title": "Application Logging",
+                "path": "guides/app_builder_guides/application_logging"
+              },
+              {
+                "title": "Configuration",
+                "path": "guides/app_builder_guides/configuration"
+              },
+              {
+                "title": "Deployment",
+                "path": "guides/app_builder_guides/deployment"
+              },
+              {
+                "title": "Development",
+                "path": "guides/app_builder_guides/development.md"
+              },
+              {
+                "title": "Distribution",
+                "path": "guides/app_builder_guides/distribution.md"
+              },
+              {
+                "title": "Events",
+                "path": "guides/app_builder_guides/events"
+              },
+              {
+                "title": "Exc App",
+                "path": "guides/app_builder_guides/exc_app"
+              },
+              {
+                "title": "Extensions",
+                "path": "guides/app_builder_guides/extensions"
+              },
+              {
+                "title": "Optimization",
+                "path": "guides/app_builder_guides/optimization.md"
+              },
+              {
+                "title": "Security",
+                "path": "guides/app_builder_guides/security"
+              },
+              {
+                "title": "Telemetry",
+                "path": "guides/app_builder_guides/telemetry.md"
+              }
+            ]
+          },
+          {
+            "title": "Runtime Guides",
+            "path": "guides/runtime_guides",
+            "pages": [
+              {
+                "title": "Asynchronous Calls",
+                "path": "guides/runtime_guides/asynchronous_calls.md"
+              },
+              {
+                "title": "Creating Actions",
+                "path": "guides/runtime_guides/creating_actions.md"
+              },
+              {
+                "title": "Creating REST APIs",
+                "path": "guides/runtime_guides/creating_rest_apis.md"
+              },
+              {
+                "title": "CI/CD Pipeline",
+                "path": "guides/runtime_guides/ci-cd_pipeline.md"
+              },
+              {
+                "title": "Debugging",
+                "path": "guides/runtime_guides/debugging.md"
+              },
+              {
+                "title": "Logging & Monitoring",
+                "path": "guides/runtime_guides/logging_monitoring.md"
+              },
+              {
+                "title": "Reference Docs",
+                "path": "guides/runtime_guides/reference_docs"
+              },
+              {
+                "title": "Security General",
+                "path": "guides/runtime_guides/security_general.md"
+              },
+              {
+                "title": "Securing Web Actions",
+                "path": "guides/runtime_guides/securing_web_actions.md"
+              },
+              {
+                "title": "System Settings",
+                "path": "guides/runtime_guides/system_settings.md"
+              },
+              {
+                "title": "Throughput Tuning",
+                "path": "guides/runtime_guides/throughput_tuning.md"
+              },
+              {
+                "title": "Tools",
+                "path": "guides/runtime_guides/tools"
+              },
+              {
+                "title": "Troubleshooting",
+                "path": "guides/runtime_guides/troubleshooting.md"
+              },
+              {
+                "title": "Using Packages",
+                "path": "guides/runtime_guides/using_packages.md"
+              },
+              {
+                "title": "Using Runtime",
+                "path": "guides/runtime_guides/using_runtime.md"
+              }
+            ]
           }
         ]
       },
